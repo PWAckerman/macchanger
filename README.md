7cb1d1414cd7cdccc67cbbeb328cce30c9054a99
PWAckerman <patrickackerman@gmail.com>
2019-05-26 19:18:55 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# MACChanger
+MACChanger is a shell script for generating a random MAC address using
+a handful of common vendor octets and replacing the MAC address on a
+specified interface (generally I use en0).
+
+This script was developed for **macOS/OSX**. I believe it will also
+work on BSD derived OSes such as **FreeBSD**.
+
+Requires both `sed` and `awk` (which you most likely have).
---
+++ macchanger.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+DEBUG=false
+declare -r ROOT_UID=0
+declare -r E_NOTROOT=87
+declare -r E_WRONG_ARGS=85
+
+declare -r VENDOR_OCTETS=("0821ef" "000393" "00a0c6" "00e0fc" "00e0f9")
+declare -r RANDOM_OCTET=$(openssl rand -hex 3)
+
+#Check for valid args
+#Only valid arg is "debug"
+if [ -z "$1" ]
+then
+  :
+else
+  if [ "$1" = "debug" ]
+  then
+    DEBUG=true
+    set -x
+  else
+    echo "Not a valid argument."
+    exit $E_WRONG_ARGS
+  fi
+fi
+
+#Abort if not root
+if [ "$UID" -ne "$ROOT_UID" ]
+then
+  echo "Must be root to run this script."
+  exit $E_NOTROOT
+fi
+
+echo "Which interface?"
+read INTERFACE
+
+CHOSEN_INDEX=$(( $RANDOM % 5 ))
+CHOSEN_VENDOR="${VENDOR_OCTETS[CHOSEN_INDEX]}"
+NEWMAC=$(echo "$CHOSEN_VENDOR$RANDOM_OCTET" | sed 's/\(..\)/\1:/g; s/.$//')
+OLDMAC=$( ifconfig "$INTERFACE" | grep ether | awk -F' ' '{print $2}')
+ifconfig "$INTERFACE" ether "$NEWMAC"
+echo $OLDMAC ">>" $NEWMAC
+exit 0
+
